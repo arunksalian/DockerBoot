eff52807bfd6b17bf91acace472c1053a15c03b5
arunksalian <arun.salian@gmail.com>
2017-08-01 19:49:00 +0530

    added dockerfile

---
+++ src/main/java/com/slin/app/DockerBoot.java
@@ -0,0 +1,15 @@
+package com.slin.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerBoot {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DockerBoot.class, args);
+		
+	}
+
+}
